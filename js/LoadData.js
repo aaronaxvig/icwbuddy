975fc5bdf566079f24b7e8ec2e7eb7b8b2f1da3e
Aaron Axvig <aaron@axvigs.com>
2019-08-29 17:09:38 -0400

    Added new GeoJson files to load in website JS

--- js/LoadData.js
+++ js/LoadData.js
@@ -8,7 +8,17 @@ var data =
 
 var aicwFiles = [
     'data/aicw-mile-marker-0-99.geojson',
-    'data/aicw-mile-marker-100-199.geojson'
+    'data/aicw-mile-marker-100-199.geojson',
+    'data/aicw-mile-marker-200-299.geojson',
+    'data/aicw-mile-marker-300-399.geojson',
+    'data/aicw-mile-marker-400-499.geojson',
+    'data/aicw-mile-marker-500-599.geojson',
+    'data/aicw-mile-marker-600-699.geojson',
+    'data/aicw-mile-marker-700-799.geojson',
+    'data/aicw-mile-marker-800-899.geojson',
+    'data/aicw-mile-marker-900-999.geojson',
+    'data/aicw-mile-marker-1000-1099.geojson',
+    'data/aicw-mile-marker-1100-1153.geojson'
 ]
 
 function fetchJSON(url) {
